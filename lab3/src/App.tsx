08e81b821e9822e6cd77e2c49dbe8b2d05e1d60f
MBarylo <matveybarilo06@gmail.com>
2026-03-06 13:24:59 +0200

    Lab 3: Add TaskCard component

--- lab3/src/App.tsx
+++ lab3/src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import TaskCard from './components/TaskCard/TaskCard';
+import type { Task } from './types/task';
+
+const mockTask: Task = {
+  id: '1',
+  title: 'Тестова задача',
+  description: 'Перевірка відображення картки',
+  status: 'todo',
+  priority: 'high',
+  createdAt: new Date(),
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState(mockTask);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <TaskCard
+      task={task}
+      onDelete={(id) => console.log('delete', id)}
+      onStatusChange={(id, status) => {
+        console.log('status callback', id, status);
+        setTask({ ...task, status }); // оновлюємо статус, React перерендерить select
+        console.log('status', id, status); // тепер буде лог
+      }}
+    />
+  );
 }
 
-export default App
+export default App;
