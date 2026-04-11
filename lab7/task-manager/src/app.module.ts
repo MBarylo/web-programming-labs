c6dafedcf06a5eb1fc258f5f12d64779cb30e3bc
MBarylo <matveybarilo06@gmail.com>
2026-04-11 14:22:09 +0300

    finish lab7

--- lab7/task-manager/src/app.module.ts
+++ lab7/task-manager/src/app.module.ts
@@ -1,6 +1,13 @@
-import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TasksModule } from './tasks/tasks.module';
+import { Module } from '@nestjs/common';
+
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TasksModule,
+  ],
 })
 export class AppModule {}
